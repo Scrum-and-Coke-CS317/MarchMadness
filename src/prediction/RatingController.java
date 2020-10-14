852cd1e8ba3d9cacbb3b03c973870757ac005930
Heimer <MHEIMER001@CSBSJU.EDU>
2020-10-13 21:56:09 -0500

    Heimer: Clean RatingController
    Get's rid of the embeded class.

--- src/prediction/RatingController.java
+++ src/prediction/RatingController.java
@@ -105,24 +105,4 @@ public class RatingController {
 			e.printStackTrace();
 		}
 	}
-
-
-	public void outputRating(ArrayList<Team> teams) {
-		Collections.sort(teams, new RankingComparator());
-	}
-}
-
-class RankingComparator implements Comparator<Team>{
-
-	public int compare(Team team1, Team team2){
-
-		if( team1.getRank() > team2.getRank() ){
-			return 1;
-		}else if( team1.getRank() > team2.getRank() ){
-			return -1;
-		}else{
-			return 0;
-		}
-
-	}
 }
