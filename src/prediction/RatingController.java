1eabcf6dfe1b1200d0d029d5dcea09a250f6c9aa
Andrew Breyen <andrew.breyen@gmail.com>
2020-10-13 19:29:42 -0500

    outputRating() and associated comparerator

--- src/prediction/RatingController.java
+++ src/prediction/RatingController.java
@@ -2,11 +2,29 @@
  * 
  */
 package prediction;
-
+import java.util.*;
 /**
  * @author zeemh
  *
  */
 public class RatingController {
 
+	public void outputRating(ArrayList<Team> teams) {
+		Collections.sort(teams, new RankingComparator());
+	}
+}
+
+class RankingComparator implements Comparator<Team>{
+    
+    public int compare(Team team1, Team team2){
+        
+        if( team1.getRank() > team2.getRank() ){
+            return 1;
+        }else if( team1.getRank() > team2.getRank() ){
+            return -1;
+        }else{
+            return 0;
+        }
+        
+    }
 }
